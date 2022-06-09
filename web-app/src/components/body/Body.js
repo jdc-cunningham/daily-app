3b0d656d31121306d7ec13a78ca2ab756de4c40f
Jacob Cunningham <jcun4128@gmail.com>
2022-06-09 15:01:10 -0500

    bound getter

--- web-app/src/components/body/Body.js
+++ web-app/src/components/body/Body.js
@@ -3,15 +3,7 @@ import './Body.scss';
 import axios from 'axios';
 
 const Body = () => {
-  const [displayInfo, setDisplayInfo] = useState({
-    daysSince: 1,
-    prettyDate: 'Thursday 06/09/2022',
-    prevDebt: '$74,000.00',
-    currentDebt: '$73,000.00'
-  });
-
-  const [wokeUp, setWokeUp] = useState(false);
-  const [weightSaved, setWeightSaved] = useState(false);
+  const [displayInfo, setDisplayInfo] = useState({});
 
   const ButtonInput = (
     <button type="button">Woke up</button>
@@ -23,7 +15,7 @@ const Body = () => {
 
   // straight outta SO
   // https://stackoverflow.com/questions/23593052/format-javascript-date-as-yyyy-mm-dd
-  const formatDate = () => {
+  const formatDate = (dashes = false) => {
     var d = new Date(),
         month = '' + (d.getMonth() + 1),
         day = '' + d.getDate(),
@@ -35,8 +27,23 @@ const Body = () => {
     if (day.length < 2) 
         day = '0' + day;
 
-    return [year, month, day].join('-');
+    const bridgeStr = dashes ? '/' : '-';
+
+    return [year, month, day].join(bridgeStr);
   };
+
+  const daysSinceStart = () => {
+    return Math.floor(
+      (Date.now() - 1654800933538) / 86400
+    );
+  }
+
+  const prettyDate = () => {
+    // https://stackoverflow.com/a/24998705/2710227
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const d = new Date();
+    return days[d.getDay()];
+  }
 
   // first run
   useEffect(() => {
@@ -45,6 +52,20 @@ const Body = () => {
     })
       .then((res) => {
         console.log(res);
+        if (res.status === 200) {
+          const { data } = res.data;
+          const firstRow = data[0];
+
+          setDisplayInfo(prevState => ({
+            ...prevState,
+            daysSince: daysSinceStart,
+            prettyDate: `${prettyDate()} ${formatDate(true)}`,
+            prevDebt: '',
+            currentDebt: '',
+            wakeUpTime: firstRow?.wake_up_time,
+            weightSaved: firstRow?.weight
+          }));
+        }
       })
       .catch((err) => {
         console.log(err);
@@ -53,15 +74,22 @@ const Body = () => {
 
   return (
     <div className="daily-app__body">
-      <div className="daily-app__body-info">
-        <h1>Day {displayInfo.daysSince}</h1>
-        <h2>{displayInfo.prettyDate}</h2>
-        <h2>Previous debt: <span className="orange">{displayInfo.prevDebt}</span></h2>
-        <h2>Current debt: <span className="red">{displayInfo.currentDebt}</span></h2>
+      <div className={`daily-app__body-info ${!displayInfo.wakeUpTime ? "center" : ""}`}>
+        {displayInfo.wakeUpTime &&
+          <>
+            <h1>Day {displayInfo.daysSince}</h1>
+            <h2>{displayInfo.prettyDate}</h2>
+            {displayInfo.prevDebt && <h2>Previous debt: <span className="orange">{displayInfo.prevDebt}</span></h2>}
+            {displayInfo.currentDebt && <h2>Current debt: <span className="red">{displayInfo.currentDebt}</span></h2>}
+          </>
+        }
+        {!displayInfo.wakeUpTime &&
+          <h1>Wake up, Jacob</h1>
+        }
       </div>
       <div className="daily-app__body-interfaces">
-        {!wokeUp && ButtonInput}
-        {wokeUp && !weightSaved && WeightInput}
+        {!displayInfo.wakeUpTime && ButtonInput}
+        {displayInfo.wakeUpTime && !displayInfo.weightSaved && WeightInput}
       </div>
     </div>
   );
