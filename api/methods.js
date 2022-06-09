d09caccecd680768eb294aedb919fac6916bdf21
Jacob Cunningham <jcun4128@gmail.com>
2022-06-09 14:13:40 -0500

    add the weight entry method

--- api/methods.js
+++ api/methods.js
@@ -19,7 +19,21 @@ const addWakeUpTimeStamp = (req, res) => {
 };
 
 const addWeight = (req, res) => {
+  const { weight, date } = req.body;
 
+  // ooh fancy, no this is actually bad
+  pool.query(
+    `${'UPDATE entries SET weight = ? WHERE date = ?'}`,
+      [weight, date],
+    (err, qRes) => {
+      if (err) {
+        console.log(err);
+        res.status(400).json({ok: false});
+      } else {
+        res.status(200).json({ok: true});
+      }
+    }
+  );
 }
 
 const getDayEntry = (req, res) => {
