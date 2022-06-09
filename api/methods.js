1557008c0177f3b11c7d4661d7c774cf54f43a88
Jacob Cunningham <jcun4128@gmail.com>
2022-06-09 14:05:14 -0500

    remove promise

--- api/methods.js
+++ api/methods.js
@@ -1,30 +1,28 @@
 const { pool } = require('./dbConnect');
 
-const addWakeUpTimeStamp = async (req, res) => {
+const addWakeUpTimeStamp = (req, res) => {
   const { date, timestamp } = req.body;
 
-  return new Promise(resolve => {
-    // ooh fancy, no this is actually bad
-    pool.query(
-      `${'INSERT INTO entries SET date = ?, wake_up_time = ?'}`,
-        [date, timestamp],
-      (err, qRes) => {
-        if (err) {
-          console.log(err);
-          resolve(false);
-        } else {
-          resolve(true);
-        }
+  // ooh fancy, no this is actually bad
+  pool.query(
+    `${'INSERT INTO entries SET date = ?, wake_up_time = ?'}`,
+      [date, timestamp],
+    (err, qRes) => {
+      if (err) {
+        console.log(err);
+        res.status(400).json({ok: false});
+      } else {
+        res.status(201).json({ok: true});
       }
-    );
-  });
+    }
+  );
 };
 
-const addWeight = async (req, res) => {
+const addWeight = (req, res) => {
 
 }
 
-const getDayEntry = async (req, res) => {
+const getDayEntry = (req, res) => {
 
 };
 
